d574c533219c94f49cfc955d038d39bdb951bcbe
Anul Agarwal <anul@sunday.gg>
2022-04-21 10:36:40 +0200

    Base Project Push

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    public static MapGenerator Instance = null;
+ 
+
+    public List<Node> nodes;
+    private void Awake()
+    {
+        Application.targetFrameRate = 100;
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {// Setup the selectedUnit's variable
+             
+        //GenerateMapVisual();
+
+    }
+   
+ 
+  
+    public float CostToEnterTile(Node sourceNode, Node targetNode)
+    {
+
+        Node n = nodes.Find(x => x == targetNode);
+       
+        float cost = 1;
+        if (sourceNode != targetNode)
+        {
+            // We are moving diagonally!  Fudge the cost for tie-breaking
+            // Purely a cosmetic thing!
+            cost += 0.001f;
+        }
+        return cost;
+    }
+
+    public List<Node> GeneratePathTo(Node s, Node n)
+    {
+        // Clear out our unit's old path.
+       //Spider clear path
+
+    
+
+        Dictionary<Node, float> dist = new Dictionary<Node, float>();
+        Dictionary<Node, Node> prev = new Dictionary<Node, Node>();
+
+        // Setup the "Q" -- the list of nodes we haven't checked yet.
+        List<Node> unvisited = new List<Node>();
+
+        Node source = nodes.Find(x => x == s);                                   
+        Node target = nodes.Find(x=> x== n);
+
+        dist[source] = 0;
+        prev[source] = null;
+        // Initialize everything to have INFINITY distance, since
+        // we don't know any better right now. Also, it's possible
+        // that some nodes CAN'T be reached from the source,
+        // which would make INFINITY a reasonable value
+        foreach (Node v in nodes)
+        {
+            if (v != source)
+            {
+                dist[v] = Mathf.Infinity;
+                prev[v] = null;
+            }
+
+            unvisited.Add(v);
+        }
+
+        while (unvisited.Count > 0)
+        {
+            // "u" is going to be the unvisited node with the smallest distance.
+            Node u = null;
+
+            foreach (Node possibleU in unvisited)
+            {
+                if (u == null || dist[possibleU] < dist[u])
+                {
+                    u = possibleU;
+                }
+            }
+
+            if (u == target)
+            {
+                break;  // Exit the while loop!
+            }
+
+            unvisited.Remove(u);
+
+            foreach (Node v in u.neighbours)
+            {
+                //float alt = dist[u] + u.DistanceTo(v);
+                float alt = dist[u] + CostToEnterTile(s,n);
+                if (alt < dist[v])
+                {
+                    dist[v] = alt;
+                    prev[v] = u;
+                }
+            }
+        }
+
+        // If we get there, the either we found the shortest route
+        // to our target, or there is no route at ALL to our target.
+
+        if (prev[target] == null)
+        {
+            // No route between our target and the source
+            return null;
+        }
+
+        List<Node> currentPath = new List<Node>();
+
+        Node curr = target;
+
+        // Step through the "prev" chain and add it to our path
+        while (curr != null)
+        {
+            currentPath.Add(curr);
+            curr = prev[curr];
+        }
+
+        // Right now, currentPath describes a route from out target to our source
+        // So we need to invert it!
+
+        currentPath.Reverse();
+
+        //selectedUnit.GetComponent<Unit>().currentPath=currentPath;
+
+
+        return currentPath;
+    }
+
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Node : MonoBehaviour {
+	public List<Node> neighbours;
+	public int x;
+	public int y;
+	
+	public Node() {
+		neighbours = new List<Node>();
+	}
+	
+	public float DistanceTo(Node n) {
+		if(n == null) {
+			Debug.LogError("WTF?");
+		}
+		
+		return Vector2.Distance(
+			new Vector2(x, y),
+			new Vector2(n.x, n.y)
+			);
+	}
+
+    private void OnMouseDown()
+    {
+		MapGenerator.Instance.GeneratePathTo(MapGenerator.Instance.nodes[2], this)[0].GetComponent<MeshRenderer>().material.color = Color.red;
+		//print(MapGenerator.Instance.nodes[0]);
+    }
+}
