ed62bb68cdc0dfb0d405fe0a7ed9b265eff3e399
Anul Agarwal <anul@sunday.gg>
2022-04-26 10:14:45 +0200

    Changes

--- Assets/Spider.cs
+++ Assets/Spider.cs
@@ -63,7 +63,25 @@ public class Spider : MonoBehaviour
         else
         {
             transform.position = Vector3.Lerp(transform.position, currentNode.transform.position, moveSpeed);
-            this.transform.LookAt(targetPostition);
+            //find the vector pointing from our position to the target
+
+
+            //_direction = (targetPostition - transform.position).normalized;
+
+            //create the rotation we need to be in to look at the target
+            //_lookRotation = Quaternion.LookRotation(_direction);
+
+            //rotate us over time according to speed until we are in the required rotation
+            // transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
+
+
+            var lookPos = targetPostition - transform.position;
+            Quaternion lookRot = Quaternion.LookRotation(lookPos);
+            lookRot.eulerAngles = new Vector3(lookRot.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Time.deltaTime * RotationSpeed);
+
+
+           // this.transform.LookAt(targetPostition);
 
         }
     }
